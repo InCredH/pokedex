a96a9d917ad769535ef12f76449233ed7345a10f
InCredH <harsh9hegishte@gmail.com>
2023-12-23 08:51:37 +0000

    (fe) fixed Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,26 +5,7 @@ import { Input,Select,Flex,Box } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Flex align="center" justifyContent="center">
-      <Input
-        variant="filled"
-        placeholder="Type a pokemon name"
-        htmlSize={60}
-        width="auto"
-        color="white"
-        mr={4}
-        ml={10}
-        mt={10}
-      />
       <Box width="150px" mt={10}>
-        <Select
-            placeholder="Select Type"
-            variant="outline"
-            color="white"
-        >
-            <option value="option1" style={{color:"black"}}>Option 1</option>
-            <option value="option2" style={{color:"black"}}>Option 2</option>
-            <option value="option3" style={{color:"black"}}>Option 3</option>
-        </Select>
       </Box>
     </Flex>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,69 @@
-//It will have a basic Logo and name of application
 import React from 'react';
 
-import { Flex } from "@chakra-ui/react";
+import { Flex, Input, Text, Center, Spacer, Select, InputGroup, InputLeftAddon } from "@chakra-ui/react";
+import { SearchIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
-  return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      padding="1rem"
-      bg="black" // Set background color to black
-      color="white" // Set text color to white
-    >Next-Gen Pokedex
-    </Flex>
-  );
+    const types = [
+        "normal",
+        "fire",
+        "water",
+        "grass",
+        "flying",
+        "fighting",
+        "poison",
+        "electric",
+        "ground",
+        "psychic",
+        "ice",
+        "bug",
+        "ghost",
+        "steel",
+        "dragon",
+        "dark",
+        "fairy"
+    ]
+    return (
+        <>
+            <Flex
+                as="nav"
+                align="center"
+                justify="space-center"
+                padding="1rem"
+                bg="black" // Set background color to black
+                color="white" // Set text color to white
+            >
+                <Center>
+                    <Text fontSize="2xl">Next-Gen Pokedex</Text>
+                </Center>
+                <Spacer />
+                <Center w="30%">
+                    <InputGroup style={{ color: "black" }}>
+                        <InputLeftAddon>
+                            <SearchIcon />
+                        </InputLeftAddon>
+                        <Input
+                            variant="filled"
+                            placeholder="Type a pokemon name" htmlSize={60}
+                            width="auto"
+                            color="white"
+                            marginX="5px"
+                        />
+                    </InputGroup>
+                </Center>
+                <Select
+                    placeholder="Select Type"
+                    variant="outline"
+                    color="white"
+                    width="20%"
+                    marginX="5px"
+                >
+                    {types.map((type) => (
+                        <option style={{ color: "black" }} > {type}</option>
+                    ))}
+                </Select>
+            </Flex >
+
+        </>
+    );
 }
